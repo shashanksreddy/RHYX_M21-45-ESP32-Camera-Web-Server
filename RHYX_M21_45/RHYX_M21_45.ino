0a4a33b9e1bf4260caf58e7c76d2241b0853fde1
shashanksreddy <76830408+shashanksreddy@users.noreply.github.com>
2025-11-20 13:02:27 +0530

    Obscure WiFi credentials in RHYX_M21_45.ino
    Replaced WiFi credentials with placeholders for security.

--- RHYX_M21_45/RHYX_M21_45.ino
+++ RHYX_M21_45/RHYX_M21_45.ino
@@ -36,8 +36,8 @@
 // ===========================
 // Enter your WiFi credentials
 // ===========================
-const char *ssid = "Probots 1F";
-const char *password = "Probots@3103";
+const char *ssid = "********";
+const char *password = "********";
 
 void startCameraServer();
 void setupLedFlash(int pin);
